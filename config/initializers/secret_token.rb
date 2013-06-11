a3526ec5b1f8f583b0ae8f34d101f65e451d39ee
Chris Diller, Hunter Gillane, Joe Yakich & Toby Rumans <pair+cdiller+hgillane+jyakich+trumans@pivotallabs.com>
2013-06-11 14:46:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlinkPivotal::Application.config.secret_token = '7bc6c5008e3b4ed009f20f777925b04d2e423a68502155100ae5de0f6da3d610fc86960d02026b9f96b93170738a72e59a7b33a45205e6a05cd10791382d92e6'
