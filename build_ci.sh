f26c118828f7e3809883b341196f1a53fab82fce
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-13 16:37:39 -0600

    remove license finder from ci build for now

--- build_ci.sh
+++ build_ci.sh
@@ -8,11 +8,8 @@ touch config/database.yml
 echo -e "test:\n adapter: sqlserver\n database: plink_test\n host: qa-plinkdb1-rds.cct4wdg0zsjg.us-east-1.rds.amazonaws.com\n username: plinkadmin\n password: ekVNMrXW_-mOfcIWXH80I9\n dsn: Provider=SQLOLEDB;Data Source=SCADA\SQLEXPRESS;UID=plinkadmin;ekVNMrXW_-mOfcIWXH80I9;Initial Catalog=plink_test;" > config/database.yml
 RAILS_ENV=test bundle exec rake db:test:prepare
 
-bundle exec license_finder
-STATUS=$?
-
 bundle exec rspec spec
-STATUS=$((STATUS + $?))
+STATUS=$?
 
 echo "The build exited with $STATUS"
 exit $STATUS
