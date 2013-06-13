322ee564cbe5cae8280cdb811bd1c075714d7947
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-13 16:25:11 -0600

    build-ci script

---
+++ build_ci.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+sudo apt-get install freetds-dev -y
+bundle install --deployment --path vendor/bundle
+touch config/database.yml
+echo -e "test:\n adapter: sqlserver\n database: plink_test\n host: qa-plinkdb1-rds.cct4wdg0zsjg.us-east-1.rds.amazonaws.com\n username: plinkadmin\n password: ekVNMrXW_-mOfcIWXH80I9\n dsn: Provider=SQLOLEDB;Data Source=SCADA\SQLEXPRESS;UID=plinkadmin;ekVNMrXW_-mOfcIWXH80I9;Initial Catalog=plink_test;" > config/database.yml
+RAILS_ENV=test bundle exec rake db:test:prepare
+
+bundle exec license_finder
+STATUS=$?
+
+bundle exec rspec spec
+STATUS=$((STATUS + $?))
+
+echo "The build exited with $STATUS"
+exit $STATUS
