bd9d4cb60e3651336d4c4e0dcd51c75c6a614250
Brian Rose and Joe Yakich <pair+brose+jyakich@pivotallabs.com>
2013-06-13 13:57:03 -0600

    Memoize current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.find(session[:current_user_id])
+    @user ||= User.find(session[:current_user_id])
   end
 end
