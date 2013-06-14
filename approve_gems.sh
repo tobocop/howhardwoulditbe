eedc5aae006e12d093b5975c65f913cfa492f549
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-14 11:25:44 -0600

    approving bundler

--- approve_gems.sh
+++ approve_gems.sh
@@ -6,3 +6,4 @@ bundle exec license_finder approve eventmachine
 bundle exec license_finder approve rails
 bundle exec license_finder approve rdoc
 bundle exec license_finder approve sqlite3
+bundle exec license_finder approve bundler
