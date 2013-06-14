b49bfe4743b1e00c7d8575ca30bb37c538955ffe
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-14 14:43:48 -0600

    taking out the database.yml command for security

--- build_ci.sh
+++ build_ci.sh
@@ -4,8 +4,6 @@ set -e
 
 sudo apt-get install freetds-dev -y
 bundle install --deployment --path vendor/bundle
-touch config/database.yml
-echo -e "test:\n adapter: sqlserver\n database: plink_test\n host: qa-plinkdb1-rds.cct4wdg0zsjg.us-east-1.rds.amazonaws.com\n username: plinkadmin\n password: ekVNMrXW_-mOfcIWXH80I9\n dsn: Provider=SQLOLEDB;Data Source=SCADA\SQLEXPRESS;UID=plinkadmin;ekVNMrXW_-mOfcIWXH80I9;Initial Catalog=plink_test;" > config/database.yml
 
 ./approve_gems.sh
 
