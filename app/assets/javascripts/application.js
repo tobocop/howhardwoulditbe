5b4064c91a04edaab7c43a97d3a35e980f3cb79b
Kristopher McDonald <kristopher@admins-MacBook-Pro-2.local>
2013-06-17 16:30:57 -0600

    started hero gallery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bjqs-1.3
 //= require foundation/foundation.js
 //= require foundation/foundation.reveal.js
 //= require custom.modernizr
