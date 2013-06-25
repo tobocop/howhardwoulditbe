142d767bafa21f62c057e9308814e162ca0fce12
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-25 16:01:10 -0600

    change CI driver declaration in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,7 +54,7 @@ Capybara.server_port = 58891
 Capybara.register_driver :selenium_with_long_timeout do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.timeout = 120
-  Capybara::Driver::Selenium.new(app, :browser => :firefox, :http_client => client)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox, :http_client => client)
 end
 
 if ENV['CI']
