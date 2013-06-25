1c7bfaa5e0f1025ce9a07f7f58f1c0eaa66c9e5c
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-25 14:32:09 -0600

    try to use longer timeout in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,3 +49,13 @@ end
 
 Capybara.app_host = "http://plink.test:58891"
 Capybara.server_port = 58891
+
+Capybara.register_driver :selenium_with_long_timeout do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.timeout = 120
+  Capybara::Driver::Selenium.new(app, :browser => :firefox, :http_client => client)
+end
+
+if ENV['CI']
+  Capybara.javascript_driver = :selenium_with_long_timeout
+end
