bc9bed08a4372225c30f5df9bcdc1c95c4c7bc13
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-06-25 08:05:45 -0600

    use new rake tasks in the ci build script

--- build_ci.sh
+++ build_ci.sh
@@ -20,8 +20,8 @@ bundle exec rake jasmine:ci
 STATUS=$((STATUS + $?))
 
 bundle exec rake db:migrate
-bundle exec rake db:test:prepare
-bundle exec rake db:create_views
+bundle exec rake db:udfs:create
+bundle exec rake db:views:create
 bundle exec rspec spec
 STATUS=$((STATUS + $?))
 
