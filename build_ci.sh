534e2cd7f3e3f8b233f4fc6c2ced2cd9a665e8f6
Brian Rose and Toby Rumans <pair+brose+trumans@pivotallabs.com>
2013-06-27 13:31:33 -0600

    adding time stamps to our build script

--- build_ci.sh
+++ build_ci.sh
@@ -4,18 +4,20 @@ export RAILS_ENV=test
 
 set -e
 
-sudo apt-get install freetds-dev -y
+sudo time apt-get install freetds-dev -y
 
-bundle install --deployment --path vendor/bundle
 
-bundle exec rake db:migrate
-bundle exec rake db:test:prepare
-bundle exec rake db:udfs:create
-bundle exec rake db:views:create
+time bundle install --deployment --path vendor/bundle
 
-./approve_gems.sh
 
-bundle exec license_finder rescan -q
+time bundle exec rake db:migrate
+time bundle exec rake db:test:prepare
+time bundle exec rake db:udfs:create
+time bundle exec rake db:views:create
+
+time ./approve_gems.sh
+
+time bundle exec license_finder rescan -q
 STATUS=$?
 
 cd gems/gigya && bundle install --deployment --path vendor/bundle && bundle exec rspec spec && bundle exec rspec integration_spec && cd ../..
