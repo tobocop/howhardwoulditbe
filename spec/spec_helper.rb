51750baf8d245eec347ca6e339b2494017b4af0b
Hunter Gillane <pair+hgillane@pivotallabs.com>
2013-07-09 10:32:29 -0600

    use capybara-webkit for acceptance tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,13 +53,4 @@ end
 
 Capybara.app_host = "http://plink.test:58891"
 Capybara.server_port = 58891
-
-Capybara.register_driver :selenium_with_long_timeout do |app|
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 120
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, :http_client => client)
-end
-
-if ENV['CI']
-  Capybara.javascript_driver = :selenium_with_long_timeout
-end
+Capybara.javascript_driver = :webkit
