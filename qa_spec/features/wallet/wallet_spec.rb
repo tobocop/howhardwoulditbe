95370c3b7e14c3c5f53f8a21e7cf219ed4321a5f
Matt Hamrick <mrhamric@gmail.com>
2013-07-10 10:35:12 -0600

    combined offers and wallet specs into one file

--- qa_spec/features/wallet/wallet_spec.rb
+++ qa_spec/features/wallet/wallet_spec.rb
@@ -3,20 +3,48 @@ require 'qa_spec_helper'
 describe "Wallet page", js: true do
   before(:each) do
     create_virtual_currency(name: 'Plink Points', subdomain: 'www')
-
     sign_up_user(first_name: "tester", email: "email@Plink.com", password: "test123")
-
     visit '/wallet'
   end
 
   subject { page }
 
-  it { pending; should have_text ('My Wallet') }
+  it { should have_text ('My Wallet') }
 
-  it 'should have three open slots initially' do
-    pending 'Waiting for implementation' do
+  context 'upon a users first visit' do
+    it 'should have three open slots initially' do
       page.should have_css('h3', text: 'This slot is empty', count: 3)
+    end
+
+    it 'should have one locked slot before an offer is completed' do
+      page.should have_css('h3', text: 'This slot is locked.')
+      page.should have_css('h3', text: 'Complete an offer to unlock this slot.')
+    end
+
+    it 'should have one locked slot before a friend is referred' do
+      pending 'Awaiting Implementation' do
+        page.should have_css('h3', text: 'This slot is locked.')
+        page.should have_css('h3', text: 'Refer a friend to unlock this slot.')
+      end
+    end
+  end
+
+  context 'before a user has linked a card' do
+    it 'should prompt the user to link a card on the offer details modal' do
+    end
+
+    it 'should not allow the user to add an offer to the wallet' do
+    end
+  end
+
+  context 'after a user has linked a card' do
+    it 'should allow the user to add an offer to the wallet' do
+    end
+
+    it 'should not allow duplicate offers in the wallet' do
+    end
+
+    it 'should allow a user to remove an offer from the wallet' do
     end
   end
 end
-
