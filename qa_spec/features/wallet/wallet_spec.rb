fcf8bf352409c734f84ccca36b5bd71e37ab6504
Matt Hamrick <mrhamric@gmail.com>
2013-07-10 15:13:09 -0600

    correct all wallet specs

--- qa_spec/features/wallet/wallet_spec.rb
+++ qa_spec/features/wallet/wallet_spec.rb
@@ -12,9 +12,6 @@ describe "Wallet page", js: true do
 
   subject { page }
 
-  it { should have_text ('My Wallet') }
-
-
   context 'upon a users first visit' do
     it 'should have three open slots initially' do
       visit '/wallet'
@@ -40,16 +37,19 @@ describe "Wallet page", js: true do
   context 'before a user has linked a card' do
     it 'should prompt the user to link a card on the offer details modal' do
       visit '/wallet'  
-      
-      # within '.modal' do
-      #   click_on 'Link Your Card'
-      # end
-      # page.should have_css '#card-add-modal'
-
+      click_on('Add to wallet', match: :first)
+      within '.modal' do
+        click_on 'Link Your Card'
+      end
+      page.should have_css '#card-add-modal'
     end
 
     it 'should not allow the user to add an offer to the wallet' do
       visit '/wallet'
+      click_on('Add to wallet', match: :first)
+      within '.modal' do
+        page.should_not have_link('Add to wallet')
+      end
     end
   end
 
@@ -63,16 +63,19 @@ describe "Wallet page", js: true do
     it 'should allow the user to add an offer to the wallet' do
       click_on('Add to wallet', match: :first)
       within '.modal' do
-        page.should have_link 'Add To My Wallet'
+        click_on 'Add To My Wallet'
       end
+      page.should have_css('h3', text: 'This slot is empty', count: 2)
+      page.should have_image('oldnavy', count: 1)
     end
-
-
-    it 'should not allow duplicate offers in the wallet' do
-    end
-
 
     it 'should allow a user to remove an offer from the wallet' do
+      click_on('Add to wallet', match: :first)
+      within '.modal' do
+        click_on 'Add To My Wallet'
+      end
+      click_on 'Remove'
+      page.should have_css('h3', text: 'This slot is empty', count: 3)
     end
   end
 end
