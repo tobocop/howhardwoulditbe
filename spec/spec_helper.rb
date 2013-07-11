9c676bde766a767dc245ef9d60b84b1fa251d5be
Hunter Gillane and Toby Rumans <pair+hgillane+trumans@pivotallabs.com>
2013-07-11 10:09:58 -0600

    try to resize browser when build is running on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,9 @@ SharedRSpecConfig.setup(RSpec.configuration)
 Capybara.app_host = "http://plink.test:58891"
 Capybara.server_port = 58891
 Capybara.javascript_driver = :webkit
+
+if ENV['CI']
+  RSpec.configuration.before(:each, type: :feature) do
+    page.driver.browser.manage.window.resize_to(1400, 1400)
+  end
+end
