f002aca8c41d28eeaffd7c3040cad8b6f61c0e81
Alex Welch and Joe Yakich <pair+awelch+jyakich@pivotallabs.com>
2013-07-12 15:55:16 -0600

    Pull the offer items into a partial and use a presenter.
    … in anticipation of sharing the view with a js template.

---
+++ app/models/offer_item_presenter.rb
@@ -0,0 +1,68 @@
+class OfferItemPresenter
+  attr_reader :offer, :virtual_currency, :view_context
+
+  def initialize(offer, options={})
+    @offer = offer
+    @virtual_currency = options.fetch(:virtual_currency)
+    @view_context = options.fetch(:view_context)
+  end
+
+  def id
+    offer.id
+  end
+
+  def name
+    offer.name.to_s
+  end
+
+  def dom_id
+    "offer_#{offer.id}"
+  end
+
+  def modal_dom_id
+    "offer-details-#{offer.id}"
+  end
+
+  def image_url
+    Plink::Config.instance.image_base_url + offer.image_url.to_s
+  end
+
+  def image_description
+    name
+  end
+
+  def max_award_amount
+    virtual_currency.amount_in_currency(offer.max_dollar_award_amount)
+  end
+
+  def currency_name
+    virtual_currency.currency_name
+  end
+
+  def tier_descriptions
+    offer.tiers_by_minimum_purchase_amount.map do |tier|
+      "Spend #{view_context.number_to_currency(tier.minimum_purchase_amount)} or more, get #{virtual_currency.amount_in_currency(tier.dollar_award_amount)} #{virtual_currency.currency_name}."
+    end
+  end
+
+  def call_to_action_link(linked, signed_in)
+    if linked
+      view_context.button_to('Add To My Wallet',
+                           view_context.wallet_offers_path(offer_id: offer.id),
+                           class: 'button primary-action narrow',
+                           data: {add_to_wallet: true, offer_dom_selector: "##{dom_id}"})
+    elsif signed_in
+      view_context.link_to("Link Your Card",
+                           view_context.account_path(link_card: true),
+                           class: 'button primary-action narrow')
+    else
+      view_context.link_to("Join Plink Today",
+                           view_context.root_path(sign_up: true),
+                           class: 'button primary-action narrow')
+    end
+  end
+
+  def description
+    Plink::StringSubstituter.gsub(offer.detail_text, offer.minimum_purchase_amount_tier, virtual_currency)
+  end
+end
---
+++ spec/models/offer_item_presenter_spec.rb
@@ -0,0 +1,131 @@
+require 'spec_helper'
+
+describe OfferItemPresenter do
+
+  let(:offer) { stub(:offer, id: 32, name: 'BK Whopper') }
+  let(:virtual_currency) { stub(:virtual_currency, currency_name: 'Plink points') }
+  let(:view_context) { stub(:fake_view_context) }
+  let(:presenter) { OfferItemPresenter.new(offer, virtual_currency: virtual_currency, view_context: view_context) }
+
+  describe 'name' do
+    it 'returns the name of the offer record' do
+      presenter.name.should == 'BK Whopper'
+    end
+  end
+
+  describe 'record_id' do
+    it 'returns the id from the offer record' do
+      presenter.id.should == 32
+    end
+  end
+
+  describe 'dom_id' do
+    it 'returns the generated dom id using the offer record' do
+      presenter.dom_id.should == 'offer_32'
+    end
+  end
+
+  describe 'modal_dom_id' do
+    it 'returns the generated dom id for the modal' do
+      presenter.modal_dom_id.should == "offer-details-32"
+    end
+  end
+
+  describe 'image_url' do
+    it 'generates the image url' do
+      #todo use this: Plink::RemoteImagePath.url_for(offer.image_url)
+
+
+      Plink::Config.instance.stub(:image_base_url).and_return('/images/')
+      offer.stub(:image_url).and_return('burger_king.png')
+      presenter.image_url.should == '/images/burger_king.png'
+    end
+
+  end
+
+  describe 'image_description' do
+    it 'is the offer name' do
+      presenter.image_description.should == 'BK Whopper'
+    end
+  end
+
+  describe 'max_award_amount' do
+    it 'returns the max award amount' do
+      offer.should_receive(:max_dollar_award_amount).and_return(20)
+      virtual_currency.should_receive(:amount_in_currency).with(20).and_return(5)
+
+      presenter.max_award_amount.should == 5
+    end
+  end
+
+  describe "currency_name" do
+    it 'returns the name of the virtual currency' do
+      presenter.currency_name.should == 'Plink points'
+    end
+  end
+
+  describe "tier_descriptions" do
+    it 'returns a collection of tier descriptions' do
+      virtual_currency.stub(:amount_in_currency).with(0.7).and_return(70)
+      virtual_currency.stub(:amount_in_currency).with(1).and_return(100)
+      fake_first_tier = stub(:fake_first_tier,
+                             minimum_purchase_amount: 5,
+                             dollar_award_amount: 0.7
+      )
+      fake_second_tier = stub(:fake_second_tier,
+                              minimum_purchase_amount: 10,
+                              dollar_award_amount: 1
+      )
+      offer.stub(:tiers_by_minimum_purchase_amount).and_return([fake_first_tier, fake_second_tier])
+      view_context.stub(:number_to_currency).with(5).and_return('$5')
+      view_context.stub(:number_to_currency).with(10).and_return('$10')
+
+      presenter.tier_descriptions.should == [
+        "Spend $5 or more, get 70 Plink points.",
+        "Spend $10 or more, get 100 Plink points."
+      ]
+    end
+  end
+
+  describe 'call_to_action_link' do
+    it 'returns the add to my wallet text if the first condition is true' do
+      view_context.should_receive(:wallet_offers_path).with({offer_id: 32}).and_return('wallet/offer/path/32')
+      view_context.should_receive(:link_to).with('Add To My Wallet',
+                                                 'wallet/offer/path/32',
+                                                 class: 'button primary-action narrow',
+                                                 data: {add_to_wallet: true, offer_dom_selector: "#offer_32" })
+
+      presenter.call_to_action_link(true, false)
+    end
+
+    it 'returns the Link Your Card text if the 2nd condition is true' do
+      view_context.should_receive(:account_path).with({link_card: true}).and_return('account/path')
+      view_context.should_receive(:link_to).with('Link Your Card',
+                                                 'account/path',
+                                                 class: 'button primary-action narrow'
+      )
+
+      presenter.call_to_action_link(false, true)
+    end
+
+    it 'returns the Join Plink Today text if both conditions are false' do
+      view_context.should_receive(:root_path).with({sign_up: true}).and_return('root/path')
+      view_context.should_receive(:link_to).with('Join Plink Today',
+                                                 'root/path',
+                                                 class: 'button primary-action narrow'
+      )
+
+      presenter.call_to_action_link(false, false)
+    end
+  end
+
+  describe 'description' do
+    it 'displays the offer text' do
+      offer.stub(detail_text: 'Earn $virtualCurrencyName$')
+      offer.stub(minimum_purchase_amount_tier: '$5')
+      Plink::StringSubstituter.should_receive(:gsub).with(offer.detail_text, offer.minimum_purchase_amount_tier, virtual_currency).and_return('Earn Plink points at this location')
+
+      presenter.description.should == 'Earn Plink points at this location'
+    end
+  end
+end
