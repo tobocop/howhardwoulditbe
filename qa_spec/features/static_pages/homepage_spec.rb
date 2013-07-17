f9398379817276b08cf4b86eb350c992db83d9be
Matt Hamrick <mrhamric@gmail.com>
2013-07-17 13:38:24 -0600

    added specs for forgotten password

--- qa_spec/features/static_pages/homepage_spec.rb
+++ qa_spec/features/static_pages/homepage_spec.rb
@@ -10,10 +10,8 @@ describe 'Logged out Home page', js: true do
 
   it {should have_text('Be a smarter shopper.') }
   it {should have_text('Join Plink and start earning rewards at over 70,000 locations nationwide.') }
-
   it {should have_link('Sign In') }
   it {should have_link('Join') } 
-  it {should have_link('Watch a short video for more info') }
   it {should have_link('Join Plink for FREE!', count: 2) }
 
   it 'should allow a guest to view all offers' do
@@ -37,7 +35,36 @@ describe 'Logged out Home page', js: true do
       within_frame 'fa7421a30fdeaa' do 
         click_on 'submit'
       end
-      # Do FB Stuff...within moda,, page should ahve text facebook
+      # Do FB Stuff...within modal, page should ahve text facebook
+    end
+  end
+
+  it 'should present the user with a link to reset their password' do
+      click_on 'Sign In'
+      page.should have_link('Forgot Password?')
+  end
+
+  context 'when a user is attempting to reset a password' do
+    before(:each) do
+
+      click_on 'Sign In'
+      click_on 'Forgot Password'
+    end
+
+    it 'should error if the email address isnt found' do
+      fill_in  'Email', with: 'fake@test.com'
+      click_on 'Send Password Reset Instructions'
+      page.should have_text 'Sorry this email is not registered with Plink.'
+    end
+
+    it 'should send the user an email if their email address is found' do
+      create_user(email: 'test@example.com', password: 'test123', first_name: 'QA')
+
+      visit '/'
+      click_on 'Sign In'
+      click_on 'Forgot Password'
+      fill_in  'Email', with: 'test@example.com'
+      page.should have_text 'Enter the email address associated with your account. We will send you an e-mail containing instructions for how you can reset your password.'
     end
   end
 end
