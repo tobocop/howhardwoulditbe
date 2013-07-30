415666a69a6cc971a90b00c2a4c2d3e2143ef874
Hunter Gillane <pair+hgillane@pivotallabs.com>
2013-07-30 16:05:35 -0600

    move partner logos urls into a helper

---
+++ app/helpers/homepage_helper.rb
@@ -0,0 +1,26 @@
+module HomepageHelper
+  def ordered_partner_logos
+    [
+      'wallet-logos/dunkindonuts.png',
+      'wallet-logos/dollargeneral.png',
+      'wallet-logos/oldnavy.png',
+      'wallet-logos/7eleven.png',
+      'wallet-logos/tacobell.png',
+      'wallet-logos/burgerking.png',
+      'wallet-logos/footlocker.png',
+      'wallet-logos/gap.png',
+      'wallet-logos/kmart.png',
+      'wallet-logos/sears.png',
+      'wallet-logos/regal.png',
+      'wallet-logos/outback.png',
+      'wallet-logos/redrobin.png',
+      'wallet-logos/arbys.png',
+      'wallet-logos/1800flowers.png',
+      'wallet-logos/quiznos.png',
+      'wallet-logos/burgerking.png',
+      'wallet-logos/tacobell.png',
+      'wallet-logos/kmart.png',
+      'wallet-logos/dunkindonuts.png'
+    ]
+  end
+end
