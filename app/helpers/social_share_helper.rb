32482348b8cc37c4ef7585da3abbb714f100d2fc
Alex Welch and Hunter Gillane <pair+awelch+hgillane@pivotallabs.com>
2013-08-02 12:06:50 -0600

    share social message body

---
+++ app/helpers/social_share_helper.rb
@@ -0,0 +1,5 @@
+module SocialShareHelper
+  def share_message_body
+    'Join Plink for free and receive a $5 gift card of your choice when you link a card.  Earn the rewards you deserve every time you shop or dine out at Plink locations.'
+  end
+end
