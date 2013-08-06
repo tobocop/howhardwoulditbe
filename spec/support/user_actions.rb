b055ad16146dfcb5f20ce330c40dcd2affe01a7b
Matt Hamrick <mrhamric@gmail.com>
2013-08-06 15:59:28 -0600

    moved eventTypeRecord and create_virtual_currency into sign_up_user method

--- spec/support/user_actions.rb
+++ spec/support/user_actions.rb
@@ -1,16 +1,23 @@
 module UserActions
   def sign_up_user(args)
+    create_event_type(name: Plink::EventTypeRecord.email_capture_type)
+    virtual_currency = create_virtual_currency(name: 'Plink Points', subdomain: 'www', exchange_rate: 100)
+
     visit '/'
 
     click_on 'Join'
-    click_on 'Join with Email'
 
-    fill_in 'First Name', with: args[:first_name]
-    fill_in 'Email', with: args[:email]
-    fill_in 'Password', with: args[:password]
-    fill_in 'Verify Password', with: args[:password]
+    within '.sign-in-modal' do
 
-    click_on 'Start Earning Rewards'
+      page.find('img[alt="Register with Email"]').click
+
+      fill_in 'First Name', with: args[:first_name]
+      fill_in 'Email', with: args[:email]
+      fill_in 'Password', with: args[:password]
+      fill_in 'Verify Password', with: args[:password]
+
+      click_on 'Start Earning Rewards'
+    end
   end
 
   def sign_in(email, password)
