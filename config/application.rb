713d7fb0ede022ec0d0a8f3a6660e5256a1c6a95
Toby Rumans <pair+trumans@pivotallabs.com>
2013-08-07 16:30:17 -0600

    filter password_confirmation from logs

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 module PlinkPivotal
   class Application < Rails::Application
     config.encoding = "utf-8"
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
     config.active_support.escape_html_entities_in_json = true
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
