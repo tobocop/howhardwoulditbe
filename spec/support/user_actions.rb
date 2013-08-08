722c5899dc9efec7ec231ba117e01432323a6140
Matt Hamrick <mrhamric@gmail.com>
2013-08-08 12:06:21 -0600

    moved redemption and validation methods into user actions

--- spec/support/user_actions.rb
+++ spec/support/user_actions.rb
@@ -28,4 +28,24 @@ module UserActions
       click_on 'Sign In'
     end
   end
+
+  def redeem_for_5(reward)     #make this non dependant on reward amount
+    pp reward.amounts
+
+    click_on 'Rewards' if current_path != '/rewards'
+    page.find('a', text: '$5').click
+    within '.modal' do
+      page.should have_content reward.name
+      click_on 'CONFIRM'
+    end
+  end
+
+  def validate_5_reward_on_account(reward, user)         #make this non dependant on reward amount
+    click_on 'My Account' if current_path != '/account'
+    page.should have_text("You have #{user.currency_balance.floor} Plink Points.", count: 2)
+    page.should have_content Date.today.to_s(:month_day)
+    page.should have_text("$5 #{reward.name}")
+    page.should have_text('-500 Plink Points')
+    page.should have_image('icon_redeem')
+  end
 end
