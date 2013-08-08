ea22d627db033f233b8bfe8669ab7eb988917ce5
Matt Hamrick <mrhamric@gmail.com>
2013-08-08 15:22:43 -0600

    Updated password reset specs to work

--- qa_spec/features/account/password_reset_spec.rb
+++ qa_spec/features/account/password_reset_spec.rb
@@ -1,27 +1,50 @@
 require 'qa_spec_helper'
 
-describe 'Password reset request', js: true do 
-  before(:each) do
-    create_user(password:'password', email: 'existing@plink.com')
-
-    visit '/'
-    click_on 'Sign In'
-    click_on 'Forgot Password?'
-  end
+describe 'Password reset request', js: true do
 
   subject { page }
 
-  it { should have_field 'Email' }
+  context 'as a Plink guest' do
+    before (:each) do
+      visit '/'
+      click_on 'Sign In'
+      with '.modal' do
+        page.should have_link 'Forgot Password?'
+        click_on 'Forgot Password?'
+      end
+    end
 
-  it 'should reject invalid emails' do
-    fill_in 'Email', with: 'invalidemail'
-    click_on 'Send Password Reset Instructions'
-    page.should have_text 'Sorry this email'
+    it { should have_text 'Forgot your password?'}
+    it { should have_text "Enter the email address associated with your account. We will send you an e-mail containing instructions for how you can reset your password." }
+
+
+    it 'should error with a blank form submission' do
+      click_ on 'Send Password Reset Instructions'
+      page.should have_text 'Sorry this email is not registered with Plink.'
+    end
+
+    it 'should error if I enter an email not associated with a Plink user record' do
+      fill_in 'Email', with: 'notamember@Plink.com'
+      click_on 'Send Password Reset Instructions'
+      page.should have_text 'Sorry this email is not registered with Plink.'
+    end
   end
 
-  it 'should sent a reset email to a valid user' do
-    fill_in 'Email', with: 'existing@plink.com'
-    click_on 'Send Password Reset Instructions'
-    page.should have_text "To reset your password, please follow the instructions sent to your email address."
+  context 'as a Plink member' do
+    before { sign_up_user('matt', 'email@plink.com', 'test123') }
+
+    it 'should send an email if a user enters a legit email address' do
+      click_on 'Log Out'
+      current_path.should == '/'
+      click_on 'Sign In'
+      within '.modal' do
+        page.should have_link 'Forgot Password?'
+        click_on 'Forgot Password?'
+      end
+      fill_in 'password_reset[email]', with: 'email@plink.com'
+      click_on 'Send Password Reset Instructions'
+      page.should have_text 'To reset your password, please follow the instructions sent to your email address.'
+      current_path.should == '/'
+    end
   end
 end
