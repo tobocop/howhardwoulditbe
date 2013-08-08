ae51eb8198115fdffe136d4443ff51ceef4fbb3c
Matt Hamrick <mrhamric@gmail.com>
2013-08-08 14:23:51 -0600

    refactored 'sign_up_user'

--- spec/support/user_actions.rb
+++ spec/support/user_actions.rb
@@ -1,5 +1,5 @@
 module UserActions
-  def sign_up_user(args)
+  def sign_up_user(first_name, email, password)
     create_event_type(name: Plink::EventTypeRecord.email_capture_type)
     virtual_currency = create_virtual_currency(name: 'Plink Points', subdomain: 'www', exchange_rate: 100)
 
@@ -8,10 +8,10 @@ module UserActions
 
     within '.sign-in-modal' do
       page.find('img[alt="Register with Email"]').click
-      fill_in 'First Name', with: args[:first_name]
-      fill_in 'Email', with: args[:email]
-      fill_in 'Password', with: args[:password]
-      fill_in 'Verify Password', with: args[:password]
+      fill_in 'First Name', with: first_name
+      fill_in 'Email', with: email
+      fill_in 'Password', with: password
+      fill_in 'Verify Password', with: password
       click_on 'Start Earning Rewards'
     end
   end
