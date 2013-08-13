8f84fa1ec76dbc34262310603c976a501a92ab56
Matt Hamrick <pair+mhamrick@pivotallabs.com>
2013-08-13 17:06:17 -0600

    updated password reset spec to work with most recent build

--- qa_spec/features/account/password_reset_spec.rb
+++ qa_spec/features/account/password_reset_spec.rb
@@ -1,34 +1,32 @@
 require 'qa_spec_helper'
 
 describe 'Password reset request', js: true do
+  before(:each) do
+    create_virtual_currency(name: 'Plink Points', subdomain: 'www', exchange_rate: 100)
+    open_password_reset_page
+  end
+
   subject { page }
 
-  context 'as a Plink guest' do
-    before(:each) do
-      open_password_reset_page
-    end
+  it { should have_text 'Forgot your password?'}
+  it { should have_text "Enter the email address associated with your account. We will "\
+    "send you an e-mail containing instructions for how you can reset your password." }
 
-    it { should have_text 'Forgot your password?'}
-    it { should have_text "Enter the email address associated with your account. We will "\
-      "send you an e-mail containing instructions for how you can reset your password." }
+  it 'should error with a blank form submission' do
+    click_on 'Send Password Reset Instructions'
+    page.should have_text 'Sorry this email is not registered with Plink.'
+  end
 
-    it 'should error with a blank form submission' do
-      click_on 'Send Password Reset Instructions'
-      page.should have_text 'Sorry this email is not registered with Plink.'
-    end
-
-    it 'should error if I enter an email not associated with a Plink user record' do
-      fill_in 'Email', with: 'notamember@Plink.com'
-      click_on 'Send Password Reset Instructions'
-      page.should have_text 'Sorry this email is not registered with Plink.'
-    end
+  it 'should error if I enter an email not associated with a Plink user record' do
+    fill_in 'Email', with: 'notamember@Plink.com'
+    click_on 'Send Password Reset Instructions'
+    page.should have_text 'Sorry this email is not registered with Plink.'
   end
 
   context 'as a Plink member' do
-    before { sign_up_user('matt', 'email@plink.com', 'test123') }
+    before { create_user( email: 'email@plink.com', password: 'test123', first_name: 'tester') }
 
     it 'should send an email if a user enters a legit email address' do
-      click_on 'Log Out'
       open_password_reset_page
       fill_in 'password_reset[email]', with: 'email@plink.com'
       click_on 'Send Password Reset Instructions'
