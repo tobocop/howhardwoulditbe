e0c56daba4d999a5ae92d9a095a636753a7eeea4
Andreas Kavountzis <pair+akavountzis@pivotallabs.com>
2013-08-19 14:29:02 -0600

    Correct helper method to handle case of 500 or less points
    [Finishes #54912844]

--- app/helpers/redemption_helper.rb
+++ app/helpers/redemption_helper.rb
@@ -1,6 +1,8 @@
 module RedemptionHelper
   def redemption_message(point_balance, currency_name)
-    if point_balance < 1000
+    if point_balance < 500
+      "You're <strong>#{sprintf('%g', 500 - point_balance)} #{currency_name}</strong> from your next reward."
+    elsif point_balance < 1000
       "You're <strong>#{sprintf('%g', 1000 - point_balance)} #{currency_name}</strong> from your next reward."
     else
       link_to "You have enough #{currency_name} to redeem for a Gift card.", rewards_path, class: 'redeem-link'
--- spec/helpers/redemption_helper_spec.rb
+++ spec/helpers/redemption_helper_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe RedemptionHelper do
   describe '#redemption_message' do
+    context 'for a user with less than 500 points' do
+      it 'returns a message indicating how many points they have until 500 points' do
+        helper.redemption_message(321, 'currency name').should == "You're <strong>179 currency name</strong> from your next reward."
+      end
+    end
     context 'for a user with less than 1000 points' do
-      it 'returns a message indicating how many points they have until 1,000' do
-        helper.redemption_message(321, 'currency name').should == "You're <strong>679 currency name</strong> from your next reward."
+      it 'returns a message indicating how many points they have until 1,000 points' do
+        helper.redemption_message(721, 'currency name').should == "You're <strong>279 currency name</strong> from your next reward."
       end
     end
     context 'for a user with >= 1000 points' do
