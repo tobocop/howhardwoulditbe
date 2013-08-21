b9698fe17deed56c1c722e15dbfedfb28d3cdc17
Joe Yakich <pair+jyakich@pivotallabs.com>
2013-08-21 16:03:55 -0600

    Make job posting open in new tab

--- app/helpers/career_helper.rb
+++ app/helpers/career_helper.rb
@@ -1,7 +1,7 @@
 module CareerHelper
   def career_items
     {
-      'Ruby on Rails Application Developer:' => "As a member of Plink's software engineering team we're looking for you to bring in proven experience building real world scalable web applications. You should be comfortable working independently and with a team of engineers who are not only passionate about the software, but the business as a whole. #{link_to 'Apply >', 'http://plink.theresumator.com/apply/eCk1dD/Ruby-Application-Developer.html', class: 'text-link'}"
+      'Ruby on Rails Application Developer:' => "As a member of Plink's software engineering team we're looking for you to bring in proven experience building real world scalable web applications. You should be comfortable working independently and with a team of engineers who are not only passionate about the software, but the business as a whole. #{link_to 'Apply >', 'http://plink.theresumator.com/apply/eCk1dD/Ruby-Application-Developer.html', class: 'text-link', target: '_blank'}"
     }
   end
 end
