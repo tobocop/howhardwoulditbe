e7828309b37f3ededab9e7288dbf931bba4624c0
Andreas Kavountzis <pair+akavountzis@pivotallabs.com>
2013-08-22 05:47:23 -0600

    Deployment scripts for NewRelic

--- config/deploy/recipes/application_keys.rb
+++ config/deploy/recipes/application_keys.rb
@@ -33,6 +33,15 @@ set_default :tango_password do
   Capistrano::CLI.password_prompt 'Tango password: '
 end
 
+# NEWRELIC:
+set_default :newrelic_license_key do
+  Capistrano::CLI.password_prompt 'NewRelic License Key: '
+end
+
+set_default :newrelic_application_name do
+  Capistrano::CLI.password_prompt 'NewRelic Application Name: '
+end
+
 namespace :application_keys do
   desc "Generate the application *.yml configuration files"
   task :setup, roles: :app do
@@ -40,6 +49,7 @@ namespace :application_keys do
     template "gigya_keys.yml.erb", "#{shared_path}/config/gigya_keys.yml"
     template "sendgrid.yml.erb", "#{shared_path}/config/sendgrid.yml"
     template "tango.yml.erb", "#{shared_path}/config/tango.yml"
+    template "newrelic.yml.erb", "#{shared_path}/config/newrelic.yml"
   end
   after "deploy:setup", "application_keys:setup"
 
@@ -48,6 +58,7 @@ namespace :application_keys do
     run "ln -nfs #{shared_path}/config/gigya_keys.yml #{release_path}/config/gigya_keys.yml"
     run "ln -nfs #{shared_path}/config/sendgrid.yml #{release_path}/config/sendgrid.yml"
     run "ln -nfs #{shared_path}/config/tango.yml #{release_path}/config/tango.yml"
+    run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
   end
   after "deploy:finalize_update", "application_keys:symlink"
 end
