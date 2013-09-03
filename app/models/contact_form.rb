c21345db4d054428922c206f9545feecd6cb3ff2
Toby Rumans <pair+trumans@pivotallabs.com>
2013-09-03 15:56:01 -0600

    Centralize Email regex

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -3,16 +3,8 @@ class ContactForm
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
-  VALID_EMAIL_REGEXP = /
-    ^[^+]+             # One or more not '+' signs
-    @                  # @ sign is mandatory
-    .+                 # One or more characters
-    \.                 # A literal dot
-    .+                 # One or more characters
-    $/x
-
   validates_presence_of :first_name, :last_name, :email, :message_text
-  validates :email, format: {with: VALID_EMAIL_REGEXP, allow_blank: true}
+  validates :email, format: {with: Plink::UserRecord::VALID_EMAIL_REGEXP, allow_blank: true}
 
   attr_accessor :first_name, :last_name, :email, :message_text, :category
 
