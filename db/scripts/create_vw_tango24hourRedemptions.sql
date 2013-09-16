1ebdea37f52741748c7ec5ac781aea85ee191dee
Toby Rumans <pair+trumans@pivotallabs.com>
2013-09-16 14:37:10 -0600

    Update tango view create script

--- db/scripts/create_vw_tango24hourRedemptions.sql
+++ db/scripts/create_vw_tango24hourRedemptions.sql
@@ -1,11 +1,10 @@
-CREATE VIEW vw_tango24hourRedemptions
-AS
+CREATE VIEW vw_tango24HourRedemptions AS
 SELECT
-  u.holdRedemptions,
+	u.holdRedemptions,
 	u.userID,
 	COUNT(tt.tangoTrackingID) AS redemptionCount,
 	SUM(tt.cardValue) AS redeemedInPast24Hours
 FROM users u
 LEFT OUTER JOIN tangoTracking tt ON tt.userID = u.userID
-	AND tt.sentToTangoOn >= getDate()-1
+	AND tt.sentToTangoOn >= getDate() - 1
 GROUP BY u.userID, u.holdRedemptions;
