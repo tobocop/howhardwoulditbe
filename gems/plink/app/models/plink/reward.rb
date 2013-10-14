8f722b818e4b87c6d8cea4ab8889a134e7497af4
Matt Hamrick <matt@plink.com>
2013-10-14 14:42:16 -0600

    Add conditional text to redemption page

--- gems/plink/app/models/plink/reward.rb
+++ gems/plink/app/models/plink/reward.rb
@@ -10,12 +10,18 @@ module Plink
       @logo_url = attributes.fetch(:logo_url)
       @amounts = award_amounts(amount_records)
       @terms = attributes[:terms]
+      @postal_mail_gift_cards = ['Subway Gift Card']
     end
+
+    def deliver_by_email?
+      !@postal_mail_gift_cards.include?(@name)
+    end
+    alias_method :deliver_by_email, :deliver_by_email?
 
     private
 
     def award_amounts(amount_records)
-      amount_records.map {|amount_record| Plink::RewardAmount.new(amount_record)}
+      amount_records.map { |amount_record| Plink::RewardAmount.new(amount_record) }
     end
   end
 end
--- gems/plink/spec/models/plink/reward_spec.rb
+++ gems/plink/spec/models/plink/reward_spec.rb
@@ -2,17 +2,9 @@ require 'spec_helper'
 
 describe Plink::Reward do
   let(:reward_amount) { new_reward_amount }
+  let(:valid_params) { { id: 100, name: 'terget', description: 'ermagherd', logo_url: 'http://example.com/logo', terms: 'onerous' } }
 
-  subject { Plink::Reward.new(
-    {
-      id: 100,
-      name: 'terget',
-      description: 'ermagherd',
-      logo_url: 'http://example.com/logo',
-      terms: 'The terms of agreement'
-    },
-    [reward_amount])
-  }
+  subject { Plink::Reward.new(valid_params, [reward_amount]) }
 
   it 'uses info from an reward record to populate all fields' do
     subject.amounts.size.should == 1
@@ -23,6 +15,20 @@ describe Plink::Reward do
     subject.name.should == 'terget'
     subject.description.should == 'ermagherd'
     subject.logo_url.should == 'http://example.com/logo'
-    subject.terms.should == 'The terms of agreement'
+    subject.terms.should == 'onerous'
+  end
+
+  describe '#deliver_by_email?' do
+
+    let(:snail_mail_gc) { Plink::Reward.new(valid_params.merge({ name: 'Subway Gift Card' }), [reward_amount]) }
+    let(:email_gc) { Plink::Reward.new(valid_params, [reward_amount]) }
+
+    it 'returns true if the card is delivered via email' do
+      email_gc.deliver_by_email?.should be_true
+    end
+
+    it 'returns false if the card is delivered via the postal service' do
+      snail_mail_gc.deliver_by_email?.should be_false
+    end
   end
 end
