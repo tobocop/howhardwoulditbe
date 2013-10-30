c27c3390662a6e4b5ba23b8bc25b2500b95e31f7
Toby Rumans <pair+trumans@pivotallabs.com>
2013-10-29 19:31:55 -0600

    Alphabetize parameters:
    	- attr_accessible
    	- alias_attribute
    	- argument lists in corresponding specs

--- gems/plink/app/models/plink/qualifying_award_record.rb
+++ gems/plink/app/models/plink/qualifying_award_record.rb
@@ -4,19 +4,19 @@ module Plink
 
     include Plink::LegacyTimestamps
 
+    alias_attribute :advertiser_id, :advertiserID
     alias_attribute :currency_award_amount, :currencyAwardAmount
     alias_attribute :dollar_award_amount, :dollarAwardAmount
+    alias_attribute :is_active, :isActive
+    alias_attribute :is_notification_successful, :isNotificationSuccessful
+    alias_attribute :is_successful, :isSuccessful
     alias_attribute :user_id, :userID
     alias_attribute :users_virtual_currency_id, :usersVirtualCurrencyID
     alias_attribute :virtual_currency_id, :virtualCurrencyID
-    alias_attribute :advertiser_id, :advertiserID
-    alias_attribute :is_active, :isActive
-    alias_attribute :is_successful, :isSuccessful
-    alias_attribute :is_notification_successful, :isNotificationSuccessful
 
-    attr_accessible :currency_award_amount, :dollar_award_amount, :user_id,
-      :users_virtual_currency_id, :virtual_currency_id, :advertiser_id, :is_active,
-      :is_successful, :is_notification_successful
+    attr_accessible :advertiser_id, :currency_award_amount, :dollar_award_amount, :is_active,
+      :is_notification_successful, :is_successful, :user_id, :users_virtual_currency_id,
+      :virtual_currency_id
 
     scope :find_successful_by_user_id, ->(user_id) {
       where('userID = ?', user_id)
--- gems/plink/spec/models/plink/qualifying_award_record_spec.rb
+++ gems/plink/spec/models/plink/qualifying_award_record_spec.rb
@@ -4,15 +4,15 @@ describe Plink::QualifyingAwardRecord do
 
   let(:valid_params) {
     {
+      advertiser_id: 23,
       currency_award_amount: 13,
       dollar_award_amount: 0.13,
+      is_active: true,
+      is_notification_successful: true,
+      is_successful: true,
       user_id: 13,
       users_virtual_currency_id: 2,
-      virtual_currency_id: 2,
-      advertiser_id: 23,
-      is_active: true,
-      is_successful: true,
-      is_notification_successful: true
+      virtual_currency_id: 2
     }
   }
 
