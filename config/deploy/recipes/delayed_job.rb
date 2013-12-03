f4eb045ba9bb172c8b1a117f73e03b7f96959bce
Toby Rumans <pair+trumans@pivotallabs.com>
2013-12-03 13:42:39 -0700

    Fix delayed job recipe for deployment

--- config/deploy/recipes/delayed_job.rb
+++ config/deploy/recipes/delayed_job.rb
@@ -2,11 +2,11 @@
 namespace :delayed_job do
   desc "Restarts our DJ default queue and named queues"
   task :restart_named_queues, roles: :app do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job --queue=default -i default_1 start"
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job --queue=default -i default_2 start"
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job --queue=intuit_account_removals -i account_removal_1 start"
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job --queue=intuit_account_removals -i account_removal_2 start"
+    run "cd #{release_path}; #{rails_env} script/delayed_job stop"
+    run "cd #{release_path}; #{rails_env} script/delayed_job --queue=default -i default_1 start"
+    run "cd #{release_path}; #{rails_env} script/delayed_job --queue=default -i default_2 start"
+    run "cd #{release_path}; #{rails_env} script/delayed_job --queue=intuit_account_removals -i account_removal_1 start"
+    run "cd #{release_path}; #{rails_env} script/delayed_job --queue=intuit_account_removals -i account_removal_2 start"
   end
   after 'deploy:update', 'delayed_job:restart_named_queues'
 end
