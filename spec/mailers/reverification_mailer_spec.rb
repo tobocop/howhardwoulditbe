637828b281893c39ae2de0bea07dacd5b2b58946
Matt Hamrick <matt@plink.com>
2013-12-03 15:03:54 -0700

    Update revivification emails to include a comma after the greeting
    [Finishes #61408122]

--- spec/mailers/reverification_mailer_spec.rb
+++ spec/mailers/reverification_mailer_spec.rb
@@ -23,7 +23,7 @@ describe ReverificationMailer do
       [email.html_part, email.text_part].each do |part|
         body = part.body.to_s
 
-        body.should =~ /Hey bobby/
+        body.should =~ /Hey bobby,/
         body.should =~ /Explain yourself!/
         body.should =~ /with a link!/
         body.should =~ /If you do not complete the steps outlined above by #{2.weeks.from_now.to_date}, your account will be removed from Plink and you will no longer earn Plink Points./
