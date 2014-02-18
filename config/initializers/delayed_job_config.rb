245b32322f5d1ae6c74cf41837e3869763f353fa
Toby Rumans <pair+trumans@plink.com>
2014-02-18 16:06:38 -0700

    Add catcher plugin for DJ to raise to exceptional
    [Finishes #65243184]

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,3 +1,5 @@
 Delayed::Worker.max_attempts = 5
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.plugins << DJExceptionalPlugin
+
---
+++ lib/dj_exceptional_plugin.rb
@@ -0,0 +1,14 @@
+require 'delayed_job'
+
+class DJExceptionalPlugin < Delayed::Plugin
+  callbacks do |lifecycle|
+    lifecycle.around(:invoke_job) do |job, *args, &block|
+      begin
+        block.call(job, *args)
+      rescue Exception => error
+        ::Exceptional::Catcher.handle("DJ Error: #{$!}")
+        raise error
+      end
+    end
+  end
+end
