9a6f98eb2259290413b2d01e4685c78852bb7236
Toby Rumans <toby.rumans@gmail.com>
2015-12-20 12:42:14 -0800

    updating deploy script

--- config/deploy/recipes/gems.rb
+++ config/deploy/recipes/gems.rb
@@ -4,7 +4,7 @@ namespace :gems do
   task :bundle_install do
     gems = ['plink', 'plink_admin', 'tango', 'gigya']
     gems.each do |gem_name|
-      run "cd #{release_path}; cd gems/#{gem_name}; RAILS_ENV=#{rails_env} bundle install --path #{shared_path}/bundle --deployment --quiet --without development test;"
+      run `cd #{release_path}; cd gems/#{gem_name}; env ARCHFLAGS="-arch x86_64" RAILS_ENV=#{rails_env} bundle install --path #{shared_path}/bundle --deployment --quiet --without development test;`
     end
   end
   after 'deploy:update_code', 'gems:bundle_install'
